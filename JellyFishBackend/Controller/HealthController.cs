6f94a13998b4a287b557087371ab5e15944194f1
Mika <mika-roos@web.de>
2023-07-31 13:18:38 +0200

    - DNS FQDNs in Settings für Docker Env und Dev Env -> hosts-dns-entries-info.txt - Release Version Vorbereitungen für Docker Env - Health Endpoint in Swagger Doc aufgenommen via HealthController Abstract und Concrete Implementierung mittels override der Get-Methode aus Abstract (da virtual) - Swagger: IOperationFilter für AuthorizeAttribute -> AllowAnonymous Attr. berücksichtig, falls  dieses an einer Methode angehangen ist wird das AuthorizeAttribute ignoriert, da die Methode ohne JWT verfügbar ist.

---
+++ JellyFishBackend/Controller/HealthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using WebApiFunction.Web.AspNet.Controller;
+
+namespace JellyFishBackend.Controller
+{
+    [Authorize(Policy = "Administrator")]
+    [ApiController]
+    [Area("jelly-api-1")]
+    [Route("[area]/[controller]")]
+    public class HealthController : WebApiFunction.Web.AspNet.Controller.HealthController
+    {
+        public HealthController(HealthCheckService healthCheckService) : base(healthCheckService)
+        {
+        }
+
+        [HttpGet]
+        public override Task<IActionResult> Get()
+        {
+            return base.Get();
+        }
+    }
+}

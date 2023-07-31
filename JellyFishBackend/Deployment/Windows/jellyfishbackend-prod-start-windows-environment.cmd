01dc9212ad887be1d37cea02662c5b6c0eaefc8d
Mika <mika-roos@web.de>
2023-07-31 13:42:37 +0200

    - Ordnerstruktur für Deployment - Script überarbeitet: jellyfishbackend-prod-start-windows-environment

--- JellyFishBackend/Deployment/Windows/jellyfishbackend-prod-start-windows-environment.cmd
+++ JellyFishBackend/Deployment/Windows/jellyfishbackend-prod-start-windows-environment.cmd
@@ -1,15 +1,20 @@
 @echo off
-C:
-cd C:\Users\Mika\Desktop\Projekte\jellyfish-messenger\JellyFishBackend\bin\Release\net7.0
+color 2
+set /p JellyfishPublishFolder=Input the folderpath of jellyfish backend:
+cd %JellyfishPublishFolder%
 set ASPNETCORE_ENVIRONMENT=Production
 set DOTNET_ENVIRONMENT=Production
 set ASPNETCORE_URLS=http://jellyfish-backend01:5030
-@echo on
+color d
 echo "[JELLYFISH-BACKEND]: Start script for Windows"
 echo "[JELLYFISH-BACKEND]: Try to start jellyfish-backend"
 echo "[JELLYFISH-BACKEND]: Minimal infrastructure requirments on host:"
+color 7
 echo "                     mysql (schemas: rest_api,jellyfish), rabbitmq"
+timeout 5 > NUL
+color 7
 dotnet JellyFishBackend.dll
+color 4
 echo "[JELLYFISH-BACKEND]: jellyfish-backend process exit, press any key to exit console"
 @echo off
 pause
